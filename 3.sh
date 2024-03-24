6dc0631e64d2c346238cdb4939264ffe94ab69ae
ABIDH E S <159527315+everbeast100@users.noreply.github.com>
2024-03-24 17:00:03 +0000

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+name=$1
+reg=$2
+m1=$3
+m2=$4
+m3=$5
+echo "Student Name:" $1
+echo "Registration NO:" $2
+echo "Marks secured in OS:" $3
+echo "Marks secured in Python:" $4
+echo "Marks secured in AI:" $5
+total=$((m1+m2+m3))
+avg=$((total/3))
+echo "Total Marks:" $total
+echo "Average:" $avg
+if [ $m1 -lt 35 ]||[ $m2 -lt 35 ]||[ $m3 -lt 35 ]
+then
+echo "FAIL"
+elif [ $avg -gt 70 ]
+then
+echo "Grade:Distinction"
+elif [ $avg -gt 60 ]
+then
+	echo "Grade: First class"
+elif [ $avg -gt 50 ]
+then
+	echo "Grade:Second class"
+else
+	echo "Pass"
+fi
